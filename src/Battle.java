355d6cee72b2a56e89b935f39aa16377d9238b46
M. Fahkry Malta <muhammadfahkry@gmail.com>
2021-04-16 14:06:31 +0700

    Update Battle.java

---
+++ src/Battle.java
@@ -0,0 +1,119 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Battle {
+    private int idxOwnEngimon;
+    private Engimon second;
+    private Inventory<Engimon> inventoryEngimon;
+    private Inventory<Skill> inventorySkill;
+
+    public Battle(int e1, Engimon e2, Inventory<Engimon> &inventoryEngimon, Inventory<Skill> &inventorySKill)
+    {
+        this.idxOwnEngimon = e1;
+        this.second = e2;
+        this.inventoryEngimon = inventoryEngimon;
+        this.inventorySkill = inventorySkill;
+    }
+
+    public int get_first()
+    {
+        return idxOwnEngimon;
+    }
+
+    public Engimon get_second()
+    {
+        return this.second;
+    }
+
+    public void set_first(int e)
+    {
+        this.idxOwnEngimon = e;
+    }
+
+    public void set_second(Engimon e)
+    {
+        this.second = e;
+    }
+
+    public float type_adv(Element el1, Element el2)
+    {
+        return el1.getAdvantage(el2);
+    }
+
+    public float sum_power(Engimon e1, List <Element> els2) //Menunggu class skill
+    {
+        int l1 = e1.get_level();
+        List <Element> els1 = e1.get_engimon_elements();
+        float maks = type_adv(els1[0], els2[0]);
+        for (int i = 0; i < els1.size(); i++)
+        {
+            for (int j = 0; j < els2.size(); j++)
+            {
+                float maks_tmp = type_adv(els1[i], els2[j]);
+                if (maks_tmp > maks)
+                {
+                    maks = maks_tmp;
+                }
+            }
+        }
+        int sum = e1.get_total_skill_power();
+        int sp = l1 * maks + sum;
+        return sp;
+
+    }
+
+    public void fight()
+    {
+        float fp1 = sum_power(inventoryEngimon.getInventory()[idxOwnEngimon], second.get_engimon_elements());
+        float fp2 = sum_power(second, inventoryEngimon.getInventory()[idxOwnEngimon].get_engimon_elements());
+    
+        System.out.println("Your Engimon Power : " + fp1);
+        System.out.println("Enemy Power : " + fp2);
+    
+        //Memberi interface ke user
+        if (fp1 > fp2)
+        {
+            win();
+        }
+        else
+        {
+            lose();
+        }
+    }
+
+    public void win()
+    {
+        //Menampilkan interface
+        System.out.println("\nYeay!!! You Win\n");
+        get_reward();
+    }
+
+    public void lose()
+    {
+        //Menampilkan interface
+        System.out.println("\nYou Lose :(\n");
+        get_penalty();
+    }
+
+    public void get_reward()
+    {
+        vector<Skill> skills = second.get_engimon_skills();
+        Skill s = skills[0];
+        inventorySkill.addInventory(s);
+        inventoryEngimon.getInventory()[idxOwnEngimon].add_exp(40);
+    
+        System.out.println("REWARD : \n");
+        System.out.println("Engimon : " + second.get_engimon_name() + " - Level " + second.get_level());
+        System.out.println("Skill : " + s.getName() + " - Mastery Level " + s.getMastery());
+        System.out.println();
+
+    }
+
+    public void get_penalty()
+    {
+        inventoryEngimon.removeInventory(idxOwnEngimon);
+        System.out.println("Your active engimon is dead!!\n\n");
+    }
+
+}
